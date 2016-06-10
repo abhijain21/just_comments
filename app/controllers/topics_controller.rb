c2b68fe0ff1d380236f2b7a3596327611a05f241
Abhishek Jain <abhishek@goodservice.in>
2016-06-10 19:15:54 +0530

    infinite scroll

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,8 +7,10 @@ class TopicsController < ApplicationController
 	def show
 		@topics = Topic.includes(:comments)
 		@topic = Topic.find(params[:id])
-
-		@comments = @topic.comments.order("created_at desc").includes(:user)
+		@comments = @topic.comments.order("created_at desc").includes(:user).paginate(page: params[:page], per_page: 10)
+	    if request.xhr?
+			render :partial=>"comments_list"
+	    end
 	end
 
 end
